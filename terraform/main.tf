3dc8ccd7f1418b71b010cbf6a6c0e5eb04a9c692
Enrico Casini <enrico.casini@vistry.ai>
2022-06-01 16:06:55 +0200

    Create testcorp-account-01 Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,38 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "testcorp" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "icochico85@gmail.com"
+    AccountName               = "testcorp-account-01"
+    ManagedOrganizationalUnit = "Testcorp"
+    SSOUserEmail              = "icochico85@gmail.com"
+    SSOUserFirstName          = "Test"
+    SSOUserLastName           = "Corp"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "icochico85@gmail.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Test Corp"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  #account_customizations_name = "testcorp-customizations"
+}
