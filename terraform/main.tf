aacd792d718eb82b331465f54022dd8a03766503
Enrico Casini <enrico.casini@vistry.ai>
2022-06-09 20:12:22 +0200

    Added imaginarycorp-account-01@vistry.ai account to Sandbox. Removed duplicated line

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,41 +61,6 @@ module "testcorp" {
   account_customizations_name = "sandbox"
 }
 
-module "testcorp" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "testcorp-account-01@vistry.ai"
-    AccountName               = "testcorp-account-01"
-    ManagedOrganizationalUnit = "TestCorp"
-    SSOUserEmail              = "testcorp-account-01@vistry.ai"
-    SSOUserFirstName          = "Test"
-    SSOUserLastName           = "Corp"
-  }
-
-  account_tags = {
-    "ABC:Owner"       = "testcorp-account-01@vistry.ai"
-    "ABC:Division"    = "ENT"
-    "ABC:Environment" = "Dev"
-    "ABC:CostCenter"  = "123456"
-    "ABC:Vended"      = "true"
-    "ABC:DivCode"     = "102"
-    "ABC:BUCode"      = "ABC003"
-    "ABC:Project"     = "123456"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "TestCorp"
-    change_reason       = "testing the account vending process"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
 module "sandboxtest" {
   source = "./modules/aft-account-request"
 
