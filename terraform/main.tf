38b3831f998859826ef906bd67e28a182fd84d0d
Enrico Casini <enrico.casini@vistry.ai>
2022-06-09 20:06:39 +0200

    Added imaginarycorp-account-01@vistry.ai account to Sandbox (test)

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,3 +60,66 @@ module "testcorp" {
 
   account_customizations_name = "sandbox"
 }
+
+module "testcorp" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "testcorp-account-01@vistry.ai"
+    AccountName               = "testcorp-account-01"
+    ManagedOrganizationalUnit = "TestCorp"
+    SSOUserEmail              = "testcorp-account-01@vistry.ai"
+    SSOUserFirstName          = "Test"
+    SSOUserLastName           = "Corp"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "testcorp-account-01@vistry.ai"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "TestCorp"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+module "sandboxtest" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "imaginarycorp-account-01@vistry.ai"
+    AccountName               = "imaginarycorp-account-01"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "imaginarycorp-account-01@vistry.ai"
+    SSOUserFirstName          = "Imaginary"
+    SSOUserLastName           = "Corp"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "imaginarycorp-account-01@vistry.ai"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Sandbox"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
