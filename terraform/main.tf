b51215738673fdc904e48b4f7862f0ef524885b0
Enrico Casini <enrico.casini@vistry.ai>
2022-07-06 16:20:03 -0700

    Added new MLOps Dev account mlops-dev@vistry.ai

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,31 @@ module "mlopsdev" {
 
   account_customizations_name = "sandbox"
 }
+
+module "mlops-dev" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "mlops-dev@vistry.ai"
+    AccountName               = "mlops-dev"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "mlops-dev@vistry.ai"
+    SSOUserFirstName          = "MLOps"
+    SSOUserLastName           = "Dev"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "mlops-dev@vistry.ai"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Sandbox"
+    change_reason       = "Adding an MLOps Dev (mlops-dev@vistry.ai) account"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
